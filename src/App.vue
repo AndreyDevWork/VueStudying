491adc9f62b475b4ec0e4845d3263fee2ca68869
Andrey <andrey.develop.a@gmail.com>
2023-06-28 19:38:25 +0700

    - Пропсы, обмен данными между родительскими и дочерники компонентами

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
+<!-- Чем слабо связаннее компоненты , тем масштабируемее будет приложение -->
 <template>
   <div class="app">
-      <form @submit.prevent>
-        <h4>Создание поста</h4>
-        <input 
-          v-bind:value="title" 
-          @input="inputTitle"
-          class="input" 
-          type="text" 
-          placeholder="Название">
-        <input 
-          v-bind:value="body" 
-          @input="body = $event.target.value"
-          class="input" 
-          type="text" 
-          placeholder="Описание">
-        <button class="btn" @click="createPost">Создать</button>
-      </form>
+    <PostForm
+      @create="createPost"
+      >
+    </PostForm>
+    <PostList 
+      :posts="posts"
+      >      
+    </PostList>
 
-    <div class="post" v-for="post in posts">
-      <div><strong>Название:</strong>{{ post.title }}</div>
-      <div><strong>Описание:</strong>{{ post.body }}</div>
-    </div>
+
   </div>
 </template>
 
 <script>
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+
 
 export default {
+  components: {
+    PostList, PostForm
+  },
   data() {
     return {
       posts: [
@@ -34,30 +30,19 @@ export default {
         {id: 2, title: 'JavaScript', body: 'Описание поста 2'},
         {id: 3, title: 'JavaScript', body: 'Описание поста 3'},
       ],
-      title: '',
-      body: '',
     }
   },
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body,
-      }
-      this.posts.push(newPost);
-      this.title = '';
-      this.body = '';
-    },
-    inputTitle(event) {
-      this.title = event.target.value;
+    createPost(post) {
+      this.posts.push(post);
     }
   }
 }
 
 </script>
-<!-- scoped стили будут примененены только к этому компоненту -->
+
 <style> 
+
 * {
   margin: 0;
   padding: 0; 
@@ -66,34 +51,6 @@ export default {
 
 .app {
   padding: 20px;
-}
-
-.post {
-  padding: 15px;
-  margin-top: 15px;
-  border: 2px solid teal;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-
-}
-
-.input {
-  width: 100%;
-  padding: 10px 15px;
-  margin-top: 15px;
-  border: 1px solid teal;
-}
-
-.btn {
-  align-self: flex-end;
-  margin-top: 15px;
-  padding: 10px 15px;
-  background: none;
-  border: 1px solid teal;
-  color: teal;
 }
 
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,33 @@
+<!-- Принимает посты и все что он делает это их отрисовывает -->
+<template>
+  <div>
+      <h3>Список пользователей</h3>
+      <post-item 
+        class="post" 
+        v-for="post in posts"
+        :post="post"
+      />
+    </div>
+    
+
+</template>
+
+
+<script>
+  import PostItem from './PostItem.vue';
+  export default {
+    props: {
+        posts: {
+            type: Array,
+            required: true
+        }
+    },
+    components: { PostItem }
+}
+</script>
+
+<!-- scoped стили будут примененены только к этому компоненту -->
+<style scoped>
+
+
+</style>
